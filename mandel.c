0cf6f23396e0da12b7eb9152814779cae73827d3
John Process <esologic@gmail.com>
2010-10-04 19:56:39 -0400

    mmap and forking

--- mandel.c
+++ mandel.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <SDL.h>
 #include <gmp.h>
-#include <math.h>
 
 static SDL_Surface *screen, *origin;
 static int screen_width, screen_height;
@@ -10,10 +14,12 @@ static mpf_t re_max;
 static mpf_t im_min;
 static mpf_t im_max;
 static mpf_t x0_inc, y0_inc;
-static int max_iters = 1000;
+static int max_iters = 200;
 static int stale = 1;
-static int last_display;
 static int start_pick_x, start_pick_y, end_pick_x, end_pick_y;
+static Uint32 *shared_data;
+static float *status;
+static int num_processes;
 
 void sdl_set_video_mode(unsigned int screen_width,
                         unsigned int screen_height,
@@ -65,7 +71,7 @@ void putpixel(int x, int y, Uint32 pixel)
     }
 }
 
-void generate_mandel()
+void run_child_generator(int process_num)
 {
     int image_x, image_y;
     mpf_t x0, y0, x, y, xt, mag2, tmp, tmp2;
@@ -79,10 +85,15 @@ void generate_mandel()
     mpf_init(tmp);
     mpf_init(tmp2);
 
-    int start_time = SDL_GetTicks();
+    /* use process_num and num_processes to determine how many columns
+     * to skip and where to start the processing */
+   
+    mpf_set(x0, re_min);   
+    mpf_mul_ui(tmp, x0_inc, process_num);
+    mpf_add(x0, x0, tmp);
+    mpf_mul_ui(x0_inc, x0_inc, num_processes);
 
-    mpf_set(x0, re_min);
-    for(image_y = 0, image_x = 0; image_x < screen_width; image_x++, image_y = 0)
+    for(image_y = 0, image_x = process_num; image_x < screen_width; image_x += num_processes, image_y = 0)
     {
         mpf_set(y0, im_min);
         for(; image_y < screen_height; image_y++)
@@ -90,7 +101,7 @@ void generate_mandel()
             int iters = 0;
             mpf_set_d(x, 0.0);
             mpf_set_d(y, 0.0);
-            
+           
             while( iters < max_iters )
             {
                 /* check for bailout */
@@ -99,7 +110,7 @@ void generate_mandel()
                 mpf_add(mag2, mag2, tmp);
                 if( mpf_cmp_ui(mag2, 4) > 0 )
                     break;
-                
+               
                 mpf_mul(tmp2, x, x);
                 mpf_mul(tmp, y, y);
                 mpf_sub(tmp, tmp2, tmp);
@@ -112,35 +123,35 @@ void generate_mandel()
                 iters++;
             }
 
-            if(iters >= max_iters)
-                putpixel(image_x, image_y, 0);
-            else {
+            if(iters >= max_iters) {
+                //putpixel(image_x, image_y, 0);
+                /* already zeroed */
+            } else {
                 double mag2_d = mpf_get_d(mag2);
                 float smooth =  iters + 1 - log(log(sqrt(mag2_d)))/log(2);
-                putpixel(image_x, image_y, SDL_MapRGB(screen->format, 0, 0, smooth * 10));
+                //putpixel(image_x, image_y, SDL_MapRGB(screen->format, 0, 0, smooth * 10));
                 //float norm_iters = (float)iters / max_iters;
                 //putpixel(image_x, image_y, SDL_MapRGB(screen->format, 0, 0, norm_iters * 256 * 200));
+
+                shared_data[image_y*screen_width + image_x] = SDL_MapRGB(screen->format, 0, 0, smooth * 10);
             }
 
-            mpf_add(y0, y0, y0_inc);
-        }
+            /* update status */
+            status[process_num] = (float)image_x / screen_width;
 
-        /* flip screen after 1 second */
-        if(SDL_GetTicks() - last_display > 1000) {
-            SDL_Flip(screen);
-            last_display = SDL_GetTicks();
+            mpf_add(y0, y0, y0_inc);
         }
 
         mpf_add(x0, x0, x0_inc);
     }
 
-    printf("time to compute = %f sec\n", ((float)SDL_GetTicks() - start_time)/1000);
+    status[process_num] = 1.0f;
 }
 
 void update_coords()
 {
     /* linearly interpolate to get coords from picked locations */
-    
+   
     mpf_t new_re_min, new_re_max;
     mpf_t new_im_min, new_im_max;
 
@@ -148,11 +159,11 @@ void update_coords()
     mpf_init(new_re_max);
     mpf_init(new_im_min);
     mpf_init(new_im_max);
-    
+   
     mpf_t tmp;
-    
+   
     mpf_init(tmp);
-    
+   
     mpf_set_d(tmp, (float)start_pick_x / screen_width);
     mpf_sub(new_re_min, re_max, re_min);
     mpf_mul(new_re_min, new_re_min, tmp);
@@ -178,7 +189,7 @@ void update_coords()
     mpf_set(im_min, new_im_min);
     mpf_set(im_max, new_im_max);
 
-    printf("new coords (%f, %f) to (%f, %f)\n", 
+    printf("new coords (%f, %f) to (%f, %f)\n",
            mpf_get_d(re_min),
            mpf_get_d(im_max),
            mpf_get_d(re_max),
@@ -212,14 +223,22 @@ void draw_box()
         putpixel(end_pick_x, y, color);
 }
 
+void copy_data()
+{
+    int w, h;
+    for(w=0; w<screen_width; w++)
+        for(h=0; h<screen_height; h++)
+            putpixel(w, h, shared_data[h*screen_width+w]);
+}
+
 int main(int argc, char *argv[])
 {
-    if(argc != 3)
+    if(argc != 4)
     {
-        printf("usage: %s <width> <height>\n", argv[0]);
+        printf("usage: %s <width> <height> <num-processes>\n", argv[0]);
         exit(1);
     }
-    
+   
     mpf_init_set_d(re_min, -2.0);
     mpf_init_set_d(re_max, 1.0);
     mpf_init_set_d(im_min, -1.0);
@@ -229,7 +248,8 @@ int main(int argc, char *argv[])
 
     screen_width = atoi(argv[1]);
     screen_height = atoi(argv[2]);
-    
+    num_processes = atoi(argv[3]);
+   
     SDL_Init(SDL_INIT_VIDEO);
 
     sdl_set_video_mode(screen_width, screen_height, 0);
@@ -240,6 +260,12 @@ int main(int argc, char *argv[])
                                   screen->format->Bmask,
                                   screen->format->Amask);
 
+    pid_t *pids = malloc(sizeof(*pids) * num_processes);
+    int shared_size = screen_width*screen_height*sizeof(Uint32) + sizeof(*status)*num_processes;
+    void *mem = mmap(0, shared_size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    shared_data = mem;
+    status = mem + screen_height*screen_width*sizeof(Uint32);
+
     int done = 0;
     int dragging = 0;
 
@@ -247,17 +273,64 @@ int main(int argc, char *argv[])
     {
         if(stale)
         {
+            int start_time = SDL_GetTicks();
+
             stale = 0;
-            last_display = SDL_GetTicks();
 
             mpf_sub(x0_inc, re_max, re_min);
             mpf_div_ui(x0_inc, x0_inc, screen_width);
             mpf_sub(y0_inc, im_max, im_min);
             mpf_div_ui(y0_inc, y0_inc, screen_height);
 
-            generate_mandel();
+            /* clear data */
+            memset(shared_data, 0, screen_height*screen_width*sizeof(Uint32));
+
+            /* fork processes */           
+            int i;
+            for (i = 0; i < num_processes; i++) {
+                pids[i] = fork();
+                switch(pids[i]) {
+                case -1:
+                    printf("problem creating child process\n");
+                    exit(1);
+                case 0:
+                    run_child_generator(i);
+                    _exit(0);
+                }
+            }
+           
+            /* periodically update screen */
+            float tot_status = 0;
+            float last_update = 0;
+            while(tot_status < 1.0)
+            {
+                for (tot_status = 0, i = 0; i < num_processes; i++)
+                    tot_status += (float)status[i] / num_processes;
+
+                /* update every 1% */
+                if(tot_status - last_update > 0.01)
+                {
+                    last_update = tot_status;
+                    
+                    copy_data();
+                    SDL_Flip(screen);
+                }
+            }
+           
+            /* wait for processes to finish */
+            int stat;
+            for (i = 0; i < num_processes; i++) {
+                waitpid(pids[i], &stat, 0);
+            }
+
+            /* one more final update */
+            copy_data();
+
+            /* final update */
             SDL_BlitSurface(screen, NULL, origin, NULL);
             SDL_Flip(screen);
+
+            printf("time to compute = %f sec\n", ((float)SDL_GetTicks() - start_time)/1000);
         }
 
         if(dragging)
@@ -266,7 +339,7 @@ int main(int argc, char *argv[])
             draw_box();
             SDL_Flip(screen);
         }
-        
+       
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -297,6 +370,8 @@ int main(int argc, char *argv[])
             }
         }
     }
+
+    free(pids);
 
     SDL_FreeSurface(origin);
     SDL_Quit();
